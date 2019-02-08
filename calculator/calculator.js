08cf62b9b9da0d6626536e8e32d2c83372f8827f
Diego Campo <diego.campo@zemoga.com>
2019-02-08 10:55:30 -0500

    html and css finished, js in progess

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,2 +1,8 @@
-let result;
-const buttons = document.querySelectorAll('.calc-button');
+// let result;
+// let buttonPressed;
+
+// const buttons = document.querySelector('.calc-pad');
+// buttons.addEventListener('click', function(event){
+//     buttonPressed = event.target.innerText;
+// });
+
